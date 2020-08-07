700864702f4c60e990d43fce4fc071b0c5d2f56c
Peter Cseh <peter.cseh@webvalto.hu>
2020-08-07 14:08:21 +0200

    Changing ports

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # nginx-prometheus-grafana
 This is a demo how to connect Nginx, Prometheus, Grafana and Python application
+
+![image](grafana\20200807_135600.PNG)
--- app/app.py
+++ app/app.py
@@ -10,10 +10,6 @@ app = Flask(__name__)
 
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 
-#config = configparser.ConfigParser()
-#config.read('config.ini')
-#return config['openweathermap']['api']
-
 api_calls = Counter(
     'api_calls',
     'Number of api calls.'
@@ -22,13 +18,14 @@ temp_change = Gauge(
     'temperature_change',
     'Temperature change')
 
+# Budapest
 lat = "47.498"
 lon = "19.0399"
 api_key = "6c0d5d6a24a282ef0bf337bc100df5c8"
 
+
 @app.route('/metrics', methods=['GET'])
 def get_weather_data():
-    """Returns all data as plaintext."""
     one_hour_back_unix = int(time.time()) - 3600
     url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&APPID={api_key}"
     print(url)
@@ -41,5 +38,6 @@ def get_weather_data():
     print(r1, r2, one_hour_back_unix)
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='172.19.0.1')
