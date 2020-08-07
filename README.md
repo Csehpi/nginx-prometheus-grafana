92e1250b6a8771258099cd376259f5c471f0160f
Peter Cseh <peter.cseh@webvalto.hu>
2020-08-07 14:58:34 +0200

    Some comments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # nginx-prometheus-grafana
 This is a demo how to connect Nginx, Prometheus, Grafana and Python application
 
-![image](grafana\20200807_135600.PNG)
+![GrafanaScreenshot](https://github.com/Csehpi/nginx-prometheus-grafana/blob/master/grafana/20200807_135600.PNG)
--- app/app.py
+++ app/app.py
@@ -1,22 +1,19 @@
 from flask import Flask, Response
 from prometheus_client import Counter, Gauge, generate_latest
 import logging
-import random
 import requests
 import time
 
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
-
 api_calls = Counter(
     'api_calls',
-    'Number of api calls.'
+    'Number of api calls with Counter'
 )
 temp_change = Gauge(
     'temperature_change',
-    'Temperature change')
+    'Temperature change with Gauge')
 
 # Budapest
 lat = "47.498"
@@ -26,17 +23,21 @@ api_key = "6c0d5d6a24a282ef0bf337bc100df5c8"
 
 @app.route('/metrics', methods=['GET'])
 def get_weather_data():
-    one_hour_back_unix = int(time.time()) - 3600
+    # Current weather data for Budapest
     url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=metric&APPID={api_key}"
-    print(url)
     r1 = requests.get(url).json()['current']['temp']
+    logger.info(f"Getting current ({int(time.time())}) weather data for Budapest. Used URL: {url}")
+
+    # One hour earlier weather data for Budapest
+    one_hour_back_unix = int(time.time()) - 3600
     url = f"https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={one_hour_back_unix}&units=metric&appid={api_key}"
-    print(url)
     r2 = requests.get(url).json()['current']['temp']
+    logger.info(f"Getting one hour earlier ({one_hour_back_unix}) weather data for Budapest. Used URL: {url}")
+
     api_calls.inc()
     temp_change.set(r1-r2)
-    print(r1, r2, one_hour_back_unix)
-    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
+    return Response(generate_latest(), mimetype=str('text/plain; charset=utf-8'))
 
 
 if __name__ == '__main__':
